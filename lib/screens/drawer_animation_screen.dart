4c89332ee304522fa5d57391b85f6a42ab1d0587
pranav-singh-parmar <71075740+pranav-singh-parmar@users.noreply.github.com>
2024-01-30 22:54:19 +0530

    Added Drawer Animation

--- lib/screens/drawer_animation_screen.dart
+++ lib/screens/drawer_animation_screen.dart
@@ -1,4 +1,5 @@
 import 'package:animations_flutter/screens/widgets/custom_app_bar.dart';
+import 'package:animations_flutter/screens/widgets/custom_drawer.dart';
 import 'package:animations_flutter/utils/app_constants.dart' show AppColors;
 import 'package:animations_flutter/utils/app_texts.dart';
 import 'package:flutter/material.dart';
@@ -13,15 +14,30 @@ class DrawerAnimationScreen extends StatefulWidget {
 class _DrawerAnimationScreenState extends State<DrawerAnimationScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: const CustomAppBar(
-          titleString: AppTexts.animatedContainerScreen,
+    return CustomDrawer(
+        drawer: Material(
+          child: Container(
+            // color: AppColors.blackColor,
+            child: ListView.builder(
+              padding: const EdgeInsets.only(left: 100, top: 100),
+              itemCount: 20,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text('Item $index'),
+                );
+              },
+            ),
+          ),
         ),
-        body: Center(
-            child: Container(
-          height: 100,
-          width: 100,
-          color: AppColors.blackColor,
-        )));
+        child: Scaffold(
+            appBar: const CustomAppBar(
+              titleString: AppTexts.drawerAnimationScreen,
+            ),
+            body: Center(
+                child: Container(
+              height: 100,
+              width: 100,
+              color: AppColors.blackColor,
+            ))));
   }
 }
