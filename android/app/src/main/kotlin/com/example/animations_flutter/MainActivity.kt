cc69547deecb6bfa77417ff7fabf3f29bc778a10
pranav-singh-parmar <71075740+pranav-singh-parmar@users.noreply.github.com>
2024-09-17 23:07:42 +0530

    Remove Project Files to update .gitignore

--- android/app/src/main/kotlin/com/example/animations_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.animations_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/utils/app_assets.dart
+++
@@ -1,4 +0,0 @@
-class AppAssets {
-  //home_screen
-  static const wallpaper = "assets/images/wallpaper.jpg";
-}
